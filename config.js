488a11e535ae65652ad3494b6d457d42083d599e
duydzkirito <85253699+duydzkirito@users.noreply.github.com>
2021-06-03 02:31:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Làm người yêu anh nhé babe ',
+    btnYes: 'Em Đồng ý <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay 7h anh qua chở em đi ăn nhá .',
     btnAccept: 'Okiiiii lun <3',
     messLink: https://www.facebook.com/profile.php?id=100021738081522 
